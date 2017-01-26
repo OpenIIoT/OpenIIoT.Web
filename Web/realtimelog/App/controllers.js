e64f57f26ef07aacc126a032121e824b2caf118b
JP Dillingham <JP@LOCAL.WHATNET.US>
2017-01-26 09:41:21 -0800

    Initial commit.

---
+++ Web/realtimelog/App/controllers.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+app.controller('testController', ['$scope', 'HubProxyFactory', function ($scope, HubProxyFactory) {
+        console.log('trying to connect to service');
+        var chat = HubProxyFactory('../signalr', 'LogHub');
+        console.log('connected to service');
+		
+        $scope.subscribe = function (fqn) { 
+			chat.invoke('subscribe'); 
+		}
+		
+        $scope.unsubscribe = function (fqn) { 
+			chat.invoke('unsubscribe', $scope.subscribeFQN); 
+		}
+
+        chat.on('read', function (fqn, item) {
+			$('#myTable > tbody:last-child').append('<tr><td>' + JSON.parse(fqn) + '</td><td>' + JSON.parse(item) + '</td></tr>');
+			window.scrollTo(0,document.body.scrollHeight);
+        })
+    }
+]);
